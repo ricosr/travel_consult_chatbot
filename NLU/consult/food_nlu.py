dcf6ee412086b6db4c7d2d84190dedc6dd88a853
rico sun <ricosr@163.com>
2019-10-04 00:51:08 +0800

    add food nlu, not done

--- NLU/consult/food_nlu.py
+++ NLU/consult/food_nlu.py
@@ -3,6 +3,23 @@
 import re
 
 
-def search_food_extract(entities):
-    pass
+def search_food_ie(customer_utterance, lac, entities):
+    ie_values_dict = {}
+    lac_inputs = {"text": customer_utterance}
+    lac_result_dict = lac.lexical_analysis(data=lac_inputs)[0]
+    food_term_tag = ["n", "nr", "nz", "PER" ]
+    return_key = False   # if extract value, it is True
+    if entities:
+        ie_values_dict["food"] = entities["value"]
+        return_key = True
+    else:
+        for tag_index in range(len(lac_result_dict["tag"])):
+            if lac_result_dict["tag"][tag_index] in food_term_tag:
+                if "v" in lac_result_dict["tag"][: tag_index] or "vd" in lac_result_dict["tag"][: tag_index] or "vn" in lac_result_dict["tag"][: tag_index]:
+                    ie_values_dict["food"] = lac_result_dict["word"][tag_index]
+                    return_key = True
+                    break
+    
+
+
 
