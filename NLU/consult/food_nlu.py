60cc5de563e77c381d35fafc8f7f55551a91110d
rico sun <ricosr@163.com>
2019-10-04 15:22:06 +0800

    update food search rule

--- NLU/consult/food_nlu.py
+++ NLU/consult/food_nlu.py
@@ -7,11 +7,16 @@ def search_food_ie(customer_utterance, lac, entities):
     ie_values_dict = {}
     lac_inputs = {"text": customer_utterance}
     lac_result_dict = lac.lexical_analysis(data=lac_inputs)[0]
-    food_term_tag = ["n", "nr", "nz", "PER" ]
+    food_term_tag = ["n", "nr", "nz", "PER"]
     return_key = False   # if extract value, it is True
     if entities:
-        ie_values_dict["food"] = entities["value"]
-        return_key = True
+        for entity in entities:
+            if entity["entity"] == "food":
+                ie_values_dict["food"] = entity["value"]
+                return_key = True
+            if entity["entity"] == "position2" or entity["entity"] == "position1":
+                ie_values_dict["restaurant"] = entity["value"]
+                return_key = True
     else:
         for tag_index in range(len(lac_result_dict["tag"])):
             if lac_result_dict["tag"][tag_index] in food_term_tag:
@@ -19,6 +24,7 @@ def search_food_ie(customer_utterance, lac, entities):
                     ie_values_dict["food"] = lac_result_dict["word"][tag_index]
                     return_key = True
                     break
+    
     
 
 
