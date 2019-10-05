aeff09337eb3a7507bb5df428b54290e62a85998
rico sun <ricosr@163.com>
2019-10-05 22:58:54 +0800

    add ask of food nlg

--- NLG/consult/food_nlg.py
+++ NLG/consult/food_nlg.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# TODO
+from random import choice
+
+
+def ask_food_restaurant():
+    response_sentences = [
+        "请问你您想吃什么？或者告诉我饭厅名字。\n例如：我想吃火锅"
+    ]
+    return choice(response_sentences)
