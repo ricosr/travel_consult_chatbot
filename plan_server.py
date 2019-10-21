f3fd99736d45016ff9b5d956f0c4b289d1b53133
rico sun <ricosr@163.com>
2019-10-21 14:10:41 +0800

    add plan server

---
+++ plan_server.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import gevent
+import zmq.green as zmq
+
+from route_control import Plan
+
+agent = Plan()
+context = zmq.Context()
+
+
+def server():
+    print("start listening ......")
+    socket = context.socket(zmq.REP)
+    socket.setsockopt(zmq.LINGER, 5000)
+    socket.bind('tcp://127.0.0.1:10010')
+    while True:
+        msg = socket.recv()
+        msg = str(msg, encoding="utf-8")
+        reply = agent.get_answer(msg, "123456")    # TODO: temp
+        # print("server:{}".format(reply))
+        socket.send_string(reply)
+
+publisher = gevent.spawn(server)
+gevent.joinall([publisher])
