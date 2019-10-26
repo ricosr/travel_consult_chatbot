bce75ecfd1922e6febadf14e17a1319ecebfb17f
rico sun <ricosr@163.com>
2019-10-26 15:34:52 +0800

    change rasa model name

--- config/config.py
+++ config/config.py
@@ -10,8 +10,8 @@ from slots.plan_slot import plan_ticket_slot
 database_address = "mongodb://xxx:xxxx@xxx.xxx.xxx.xxx:27017/admin"
 database_name = "Travel_DB"
 
-intent_model_name = "models5"
-confirm_model_name = "confirm_model"
+intent_model_name = "models6"
+confirm_model_name = "confirm_model2"
 
 handle_config = {
     'consult_food': consult_food_handle,
