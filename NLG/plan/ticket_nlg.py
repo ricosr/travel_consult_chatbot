cc863028d9b6e04e9a15cae250e9f451dec68271
rico sun <ricosr@163.com>
2019-10-26 15:38:13 +0800

    improve ticket nlg

--- NLG/plan/ticket_nlg.py
+++ NLG/plan/ticket_nlg.py
@@ -5,28 +5,35 @@ from random import choice
 
 def ask_depart_dest_vehicle_date():  # TODO
     response_sentences = [
-        "请问你的出发时间，出发城市，目的城市，计划乘坐什么交通工具？\n例如：8月8号坐飞机从北京去上海"
+        "请问您的出发时间，出发城市，目的城市，计划乘坐什么交通工具？\n例如：8月8号坐飞机从北京去上海"
     ]
     return choice(response_sentences)
 
 
 def ask_depart():
     response_sentences = [
-        "请问你的出发城市是哪里\n例如：从北京出发"
+        "请问您的出发城市是哪里？\n例如：从北京出发"
     ]
     return choice(response_sentences)
 
 
 def ask_dest():
     response_sentences = [
-        "请问你的目的城市是哪里\n例如：到上海"
+        "请问您的目的城市是哪里？\n例如：到上海"
     ]
     return choice(response_sentences)
 
 
 def ask_vehicle():
     response_sentences = [
-        "请问你的交通方式是什么\n例如：火车, 飞机, 客车"
+        "请问您的交通方式是什么？\n例如：火车, 飞机, 客车"
+    ]
+    return choice(response_sentences)
+
+
+def ask_departure_date():
+    response_sentences = [
+        "请问您的出发日期是几号？\n例如：2020年1月1日"
     ]
     return choice(response_sentences)
 
