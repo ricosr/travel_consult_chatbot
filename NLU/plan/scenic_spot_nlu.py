684a5f2d6e9bf4dcab8379f9a502600e99d450af
rico sun <ricosr@163.com>
2019-10-29 17:32:12 +0800

    continue scenic spot nlu

--- NLU/plan/scenic_spot_nlu.py
+++ NLU/plan/scenic_spot_nlu.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import re
 
 from slots.plan_slot import plan_scenic_spot_slot
 from NLU.common import confirm_nlu
 # from plan_nlu_key_terms import plan_ticket_key_terms
 
 
-departure_destination_term_tag = ["LOC", "ORG", "PER", "ns", "nr", "nz", "f", "s", "nt", "nw"]  # n
+city_term_tag = ["LOC", "ORG", "PER", "ns", "nr", "nz", "f", "s", "nt", "nw"]  # n
+departure_time_term_tag = ["m", "q", "TIME", "t"]
 
 
 def judge_all_entities(ie_values_dict):
@@ -26,9 +28,20 @@ def paddle_lac(text, lac):
 
 
 def convert_to_num(utterance):
-    num_dict = {"一": "1", "二": "2", "两": "2", "三": "3", "四": "4", "五": "5", "六": "6", "七": "7", "八": "8", "九": "9"}
+    large_num = ['十', '百', '千', '万']
+    for num in large_num:
+        if num in utterance:
+            return False
+    re_pattern = re.compile("[0-9]+")
+    re_result = re_pattern.search(utterance)
+    if int(re_result.group(0)) > 7:
+        return False
+    num_dict = {"一": "1", "二": "2", "两": "2", "三": "3", "四": "4", "五": "5", "六": "6", "七": "7", "八": "8", "九": "9", "零": "0"}
     for key, value in num_dict.items():
         if key in utterance:
             utterance = utterance.replace(key, value)
     return utterance
 
+
+def ie_all_plan_scenic_spot(customer_utterance, lac, entities, ask_type=None):
+    lac_result_dict = paddle_lac(customer_utterance, lac)
