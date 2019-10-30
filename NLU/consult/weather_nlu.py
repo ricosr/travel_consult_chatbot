22657d0e33da2e7d91b822ca3f7c4ee6002c686f
rico sun <ricosr@163.com>
2019-10-30 23:29:04 +0800

    weather nlu done

--- NLU/consult/weather_nlu.py
+++ NLU/consult/weather_nlu.py
@@ -21,8 +21,9 @@ def ie_city_date(customer_utterance, city_ls):
     for city in city_ls:
         if city in customer_utterance:
             ie_values_dict["city"] = city
-    for day_key, day_val in consult_weather_key_terms:
+    for day_key, day_val in consult_weather_key_terms.items():
         if day_key in customer_utterance:
+            print(day_val, customer_utterance)
             ie_values_dict["date"] = day_val
     return ie_values_dict
 
@@ -30,6 +31,7 @@ def ie_city_date(customer_utterance, city_ls):
 def confirm_consult_weather(customer_utterance, confirm_interpreter, senta_gru, city_ls):
     ie_slot_result = ie_city_date(customer_utterance, city_ls)
     if ie_slot_result:
+        print("nlu change", ie_slot_result)
         return "change", ie_slot_result
     confirm_state = confirm_nlu.judge_confirm_classification(customer_utterance, senta_gru, confirm_interpreter)
     print(confirm_state)
