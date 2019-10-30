2256f391f0dfc80cb9acad1d2ae6f626cc115379
rico sun <ricosr@163.com>
2019-10-30 13:39:23 +0800

    update confirm terms, config and bug recorde

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ database_address = "mongodb://xxx:xxxx@xxx.xxx.xxx.xxx:27017/admin"
 database_name = "Travel_DB"
 
 intent_model_name = "models7"
-confirm_model_name = "confirm_model5"
+confirm_model_name = "confirm_model6"
 
 handle_config = {
     'consult_food': consult_food_handle,
@@ -39,4 +39,4 @@ db_collection_config = {   # TODO: database is not done
     "plan_scenic_spot": ""
 }
 
-plan_intent_ls = ["plan_ticket"]    # TODO: "plan_scenic_spot", "plan_hotel"
+plan_intent_ls = ["plan_scenic_spot"]    # TODO: "plan_ticket", "plan_hotel"
