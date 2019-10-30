1dc3401ec01ce7bb732267a3fb7ced74cf69a9ef
rico sun <ricosr@163.com>
2019-10-30 23:32:23 +0800

    add weather config

--- config/config.py
+++ config/config.py
@@ -2,9 +2,10 @@
 
 from flow_structure.consult.consult_food_flow import consult_food_handle
 from flow_structure.consult.consult_traffic_flow import consult_traffic_handle
+from flow_structure.consult.consult_weather_flow import consult_weather_handle
 from flow_structure.plan.plan_ticket_flow import plan_ticket_handle
 from flow_structure.plan.plan_scenic_spot_flow import plan_scenic_spot_handle
-from slots.consult_slot import dinning_slot, consult_food_slot, consult_traffic_slot
+from slots.consult_slot import consult_food_slot, consult_traffic_slot, consult_weather_slot
 from slots.plan_slot import plan_ticket_slot, plan_scenic_spot_slot
 
 
@@ -17,6 +18,7 @@ confirm_model_name = "confirm_model6"
 handle_config = {
     'consult_food': consult_food_handle,
     'consult_traffic': consult_traffic_handle,
+    'consult_weather': consult_weather_handle,
     'plan_ticket': plan_ticket_handle,
     'plan_scenic_spot': plan_scenic_spot_handle
 }
@@ -24,6 +26,7 @@ handle_config = {
 slot_config = {
     'consult_food': consult_food_slot,
     'consult_traffic': consult_traffic_slot,
+    'consult_weather': consult_weather_slot,
     'plan_ticket': plan_ticket_slot,
     'plan_scenic_spot': plan_scenic_spot_slot
 }
@@ -35,6 +38,7 @@ slot_config = {
 db_collection_config = {   # TODO: database is not done
     'consult_food': "restaurant",
     'consult_traffic': "",
+    'consult_weather': "",
     'plan_ticket': "",
     "plan_scenic_spot": ""
 }
