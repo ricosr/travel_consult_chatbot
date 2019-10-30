34198534d276e974bff87ff526c7c431a7784d5e
rico sun <ricosr@163.com>
2019-10-30 16:57:28 +0800

    add make weather consult

--- data/combine_md.py
+++ data/combine_md.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-version = 5
+version = 8
 
 with open("food/food_train_data{}.md".format(version), 'r', encoding="utf-8") as fpr1:
     food_train_ls = fpr1.readlines()
@@ -14,11 +14,16 @@ with open("shopping/shopping_train_data{}.md".format(version), 'r', encoding="ut
 with open("traffic/traffic_train_data{}.md".format(version), 'r', encoding="utf-8") as fpr4:
     traffic_train_ls = fpr4.readlines()
 
+with open("weather/weather_train_data{}.md".format(version), 'r', encoding="utf-8") as fpr5:
+    weather_train_ls = fpr5.readlines()
+
 with open("../intent/nlu{}.md".format(version), 'w', encoding="utf-8") as fpw:
     fpw.writelines(food_train_ls)
     fpw.write('\n')
-    fpw.writelines(hotel_train_ls)
-    fpw.write('\n')
-    fpw.writelines(shopping_train_ls)
-    fpw.write('\n')
     fpw.writelines(traffic_train_ls)
+    fpw.write('\n')
+    fpw.writelines(weather_train_ls)
+    # fpw.write('\n')
+    # fpw.writelines(hotel_train_ls)
+    # fpw.write('\n')
+    # fpw.writelines(shopping_train_ls)
