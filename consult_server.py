ca5f66532ffecdb879631a25726e7d09f84b34f5
rico sun <ricosr@163.com>
2019-10-31 23:52:18 +0800

    update solving receiving message function of consult and plan server

--- consult_server.py
+++ consult_server.py
@@ -15,9 +15,10 @@ def server():
     socket.setsockopt(zmq.LINGER, 5000)
     socket.bind('tcp://127.0.0.1:10086')
     while True:
-        msg = socket.recv()
-        msg = str(msg, encoding="utf-8")
-        reply = agent.get_answer(msg, "123456")    # TODO: temp
+        recv_msg = socket.recv()
+        recv_msg = str(recv_msg, encoding="utf-8")
+        msg, user_id = recv_msg.split("@---@")
+        reply = agent.get_answer(msg, user_id)
         # print("server:{}".format(reply))
         socket.send_string(reply)
 
