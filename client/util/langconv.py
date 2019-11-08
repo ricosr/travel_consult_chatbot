f7a94fbccbd8a9d5d647b7ff1720b3f76a918a5e
rico sun <ricosr@163.com>
2019-11-08 17:33:25 +0800

    fix import bug for langconv

--- client/util/langconv.py
+++ client/util/langconv.py
@@ -11,7 +11,7 @@ except:
     pass
 
 try:
-    from zh_wiki import zh2Hant, zh2Hans
+    from util.zh_wiki import zh2Hant, zh2Hans
 except ImportError:
     from zhtools.zh_wiki import zh2Hant, zh2Hans
 
