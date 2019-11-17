63124447518d472482d8ab512d78187fadedc2ea
rico sun <ricosr@163.com>
2019-11-18 01:38:59 +0800

    update consult nlg

--- NLG/consult/food_nlg.py
+++ NLG/consult/food_nlg.py
@@ -5,7 +5,7 @@ from random import choice
 
 def ask_food_restaurant():  # TODO
     response_sentences = [
-        "请问你您想吃什么？或者告诉我饭厅名字。\n例如：我想吃火锅"
+        "请问你您想吃什么？或者告诉我饭厅名字。\n例如：我想吃烤肉"
     ]
     return choice(response_sentences)
 
